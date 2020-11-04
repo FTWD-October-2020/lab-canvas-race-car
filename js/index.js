d70d257b0373bd8b14871e7695bb6f264188c44c
Steven Sarria <ssarr012@fiu.edu>
2020-11-04 17:27:31 -0500

    stevens iteration

--- js/index.js
+++ js/index.js
@@ -3,10 +3,37 @@ const ctx = canvas.getContext('2d')
 
 let road = new Image()
 road.src = "images/road.png"
-let car = new Image()
-car.src = 'images/car.png'
+let carImg = new Image()
+carImg.src = 'images/car.png'
+
+let car = {
+  x: 225,
+  y: 600,
+  w: 80,
+  h: 100,
+}
+
+function drawCar() {
+  ctx.drawImage(carImg, car.x, car.y, car.w, car.h)
+}
+
+window.onkeydown = function (e) {
+  if (e.key === "ArrowLeft") {
+    car.x -= 10;
+  } else if (e.key === "ArrowRight") {
+    car.x += 10;
+  }
+
+}
+
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(road, 0, 0, canvas.width, canvas.height)
+  drawCar()
+}
 
 document.getElementById('start-button').onclick = () => {
-  ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(car, canvas.width / 2.4, canvas.height - 150, canvas.width * .15, canvas.height * .2)
+  animate()
 }
