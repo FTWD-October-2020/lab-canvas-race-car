8b89ab8b7fc178ef54f5a130fc4743de64cb4df8
Alejandro Mojarro <amojarrot@gmail.com>
2020-11-04 14:59:46 -0700

    alex

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ const ctx = canvas.getContext('2d')
 
 let road = new Image()
 road.src = "images/road.png"
+let car = new Image()
+car.src = 'images/car.png'
 
 document.getElementById('start-button').onclick = () => {
   ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(car, canvas.width / 2.4, canvas.height - 150, canvas.width * .15, canvas.height * .2)
 }
